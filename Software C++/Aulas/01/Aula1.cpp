f6330a3872a8b85075c7a18ffa4b454489dd7bae
Hendrick Serafim <heendrick@live.com>
2025-03-25 20:20:44 -0300

    atualizando

---
+++ Software C++/Aulas/01/Aula1.cpp
@@ -0,0 +1,16 @@
+//incerssão de biblioteca sempre.
+//biblioteca I/O stream para entrada e saída de dados.
+//objetivo deste programa é mostrar a estrutura basica para a criação de uma arquivo em C++.
+
+#include <iostream>
+
+//usando o namespace std para não precisar ficar digitando std:: antes de cada comando.
+using namespace std;
+
+// int é o tipo de retorno da função main, que é um inteiro.
+// void é o tipo de retorno da função main, que é vazio.
+
+// função principal do programa.
+int main(){ return 0;} 
+
+
